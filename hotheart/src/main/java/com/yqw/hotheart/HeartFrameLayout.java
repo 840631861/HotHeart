a026d39c096c6990ddb1ea06198c655901f43757
张中文 <840631861@qq.com>
2020-05-19 16:45:34 +0800

    在viewgroup中来回滑动报错

--- hotheart/src/main/java/com/yqw/hotheart/HeartFrameLayout.java
+++ hotheart/src/main/java/com/yqw/hotheart/HeartFrameLayout.java
@@ -88,6 +88,8 @@ public class HeartFrameLayout extends FrameLayout {
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
+        if(list == null)
+            list = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             HeartBean heartBean = list.get(i);
             // 重置
